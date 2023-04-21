98af72f9f9f5de80f69f1033a6059746a6bd663c
Jakson Casas <jaksoncasas@gmail.com>
2023-04-21 17:05:35 -0500

    add marker

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
-  ]
+    `
+      .mapa-container {
+        height: 100%;
+        width: 100%;
+      }
+    `,
+  ],
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [-76.3873522392868, -13.077623610426357];
+  @ViewChild('mapa') divMapa!: ElementRef;
+  constructor() {}
 
-  constructor() { }
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
 
-  ngOnInit(): void {
+    //Agregar marcador
+    const marker = new mapboxgl.Marker()
+      .setLngLat(this.center)
+      .addTo(this.mapa);
   }
-
 }
