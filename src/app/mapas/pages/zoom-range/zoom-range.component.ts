b33cc7fe148a4cee2a5111779d533b0118348c55
Jakson Casas <jaksoncasas@gmail.com>
2023-04-21 08:01:55 -0500

    add container zoom range

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
-  ]
+    `
+      .mapa-container {
+        height: 100%;
+        width: 100%;
+      }
+      .row {
+        background-color: white;
+        border-radius: 15px;
+        position: fixed;
+        left: 50px;
+        padding: 10px;
+        bottom: 50px;
+        z-index: 999;
+        background-color: #ffffff9c;
+        backdrop-filter: blur(3px);
+        -webkit-backdrop-filter: blur(3px);
+      }
+    `,
+  ],
 })
 export class ZoomRangeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-76.3873522392868, -13.077623610426357],
+      zoom: 17,
+    });
   }
-
 }
